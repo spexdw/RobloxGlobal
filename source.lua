d8fcd5d9f291abdbd3ab66f2d9ab7e5924f5c64c
SpeX <real.tar1k61@gmail.com>
2024-11-25 14:44:53 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -2691,7 +2691,7 @@ do
 
     Library.Watermark = WatermarkOuter;
     Library.WatermarkText = WatermarkLabel;
-    Library:MakeDraggable(Library.Watermark);
+    --Library:MakeDraggable(Library.Watermark);
 
 
 
